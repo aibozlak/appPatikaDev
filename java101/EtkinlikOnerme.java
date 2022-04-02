d3adb25ad3977f2ab3fed9558a87c70cdd8072cd
Ali Bozlak <86133371+aibozlak@users.noreply.github.com>
2022-04-02 21:41:00 +0300

    Add files via upload

---
+++ java101/EtkinlikOnerme.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+// @author: app.patika.dev/aibozlak
+
+public class EtkinlikOnerme {
+ public static void main(String[] arguments){
+
+System.out.println("\nHava Sıcaklığına Göre Etkinik Önerme");
+Scanner input = new Scanner(System.in);
+
+System.out.print("\nHava sıcaklığını giriniz: ");
+int heat = input.nextInt();
+
+if (heat < 5){
+ System.out.println("\nKayak yapmayı düşünebilirsiniz");
+} else if (heat < 10){
+ System.out.println("\nSinemaya gitmeyi düşünebilirsiniz");
+} else if (heat < 15){
+ System.out.println("\nSinemaya gitmeyi veya piknik yapmayı düşünebilirsiniz");
+}  else if (heat < 25){
+ System.out.println("\nPiknik yapmayı düşünebilirsiniz");
+} else {
+ System.out.println("\nYüzme düşünebilirsiniz");
+}
+
+ }
+}
