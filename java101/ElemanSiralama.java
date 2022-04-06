272955a7b3f01605a004ca5108ecbf3bec8b7bb7
Ali Bozlak <86133371+aibozlak@users.noreply.github.com>
2022-04-06 22:45:42 +0300

    Kullanıcıdan alınan sayıları sıralama

---
+++ java101/ElemanSiralama.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+/* 
+ * app.patika.dev/aibozlak
+ * Ödev: Java dilinde, dizideki elemanları küçükten büyüğe sıralayan programı yazınız. 
+ *       Dizinin boyutunu ve dizinin elemanlarını kullanıcıdan alınız.
+*/
+
+public class ElemanSiralama {
+
+   // Girilen bir dizide giren bir değerden var mı diye kontrol fonsiyonu:
+   static boolean suDizideTekrariVar(int[] array, int value){
+      for (int eleman : array){
+         if(eleman == value){ return true; }
+      }
+      return false;
+   }
+
+   static int[] kucuktenBuyugeSirala(int[] array){
+      int[] siralanmisDizi = new int[array.length];
+      int iSiralanmisDizi = 0;
+      for (int eleman : array){
+         int minDeger = Integer.MAX_VALUE;
+         for (int i = 0; i < array.length; i++){
+            if (array[i] < minDeger && suDizideTekrariVar(siralanmisDizi,array[i]) == false){ minDeger = array[i]; }
+         }         
+         siralanmisDizi[iSiralanmisDizi] = minDeger;
+         iSiralanmisDizi++;
+      }
+
+      return siralanmisDizi;
+   }
+
+	public static void main(String[] arguments) {
+
+		Scanner input = new Scanner(System.in);	
+      int n;
+		do{
+         System.out.print("Gireceğiniz eleman sayısı: ");
+         n = input.nextInt();
+         if (n < 1){ System.out.println("Eleman sayısı 0'dan büyük doğal sayı olamalı!"); }
+      } while (n < 1);
+
+      int[] sayilar = new int[n];
+      for (int i = 0; i < n; i++){
+         System.out.print((i+1) + ". tamsayıyı giriniz: ");
+         sayilar[i] = input.nextInt();
+      }
+
+      int[] siralanmisSayilar = kucuktenBuyugeSirala(sayilar);
+      System.out.println("Girdiğiniz sayılar küçükten büyüğe: ");
+      for (int eleman : siralanmisSayilar){
+         System.out.print(eleman + "  ");
+      }
+
+	}
+
+}
